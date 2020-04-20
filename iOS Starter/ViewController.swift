0bec0bdb1d6c1e98b479aa4b7c47a299e1b0e48f
Rodrigo Gontijo <rodrigo.gontijo@hotmail.com>
2020-04-20 17:19:22 -0300

    First commit

---
+++ iOS Starter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS Starter
+//
+//  Created by Rodrigo Augusto Gontijo on 20/04/2020.
+//  Copyright © 2020 Rodrigo Augusto Gontijo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
